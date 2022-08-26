d48442b778e053c96130c4bad956b15cff9ba4da
Tegisty Hailay <tigisthay13@gmail.com>
2022-08-27 00:12:38 +0300

    pagesfor telecom user analysis

---
+++ home.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join('..')))
+
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import streamlit as st
+
+from script import utils, clean_data
+
+def run():
+  st.write("## Introduction")
+  st.write("This project is performed to analyse whether Tellco is worth buying and selling.\
+           To perform the project the data extracted from a month of aggregated data on xDR is used.\
+           In this work, data cleaning, transforming, exploration and analysis tasks are performed.\
+           Finally,user overview, engagement, experience and satisfaction analytics is done and the Telcco productivity is predicted")
+  st.write("## The following Analysis are performed")
+  st.write("1. Marketing Analysis")
+  st.write("2. User Enagagement Analysis")
+  st.write("3. User Experiance Analysis")
+  st.write("4. User Satisfaction Analysis")
+  
+
+  
